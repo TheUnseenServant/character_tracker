04bc16dee9ae479333518dfb4b1b2146a228d387
Leam Hall <leamhall@gmail.com>
2024-10-29 19:02:52 -0500

    Working on parser.

--- parse_data.py
+++ parse_data.py
@@ -11,17 +11,29 @@ input_file = "data/raw_data.txt"
 
 markup = ["[hr]", "[i]", "[/i]", "[spoiler]", "[/spoiler]", "[b]", "[/b]", "."]
 
+def list_from_file(file):
+    """ Take a file, return the valid lines as a list """
+    data = []
+    with open(file, 'r') as f:
+       for line in f:
+            line = scrub_line(line, markup)
+            if line:
+                data.append(line) 
+    return data
 
-def scrub_line(line):
+
+def scrub_line(line, markup = markup):
+    """ Remove specified markup. """
     for mark in markup:
-        if mark in line:
+        if mark.lower() in line.lower():
             line = line.replace(mark, "")
 
     return line.strip()
 
 
 if __name__ == "__main__":
-    with open(input_file, "r") as in_f:
-        for line in in_f.readlines():
-            line = scrub_line(line)
-            print(line)
+    data = list_from_file(imput_file, markup)
+    
+    for line in data:
+        print(line)
+
--- test/test_parse_data.py
+++ test/test_parse_data.py
@@ -15,14 +15,6 @@ import parse_data as pd
 
 
 class TestScrub(unittest.TestCase):
-    def setUp(self):
-        self.test_dir = tempfile.TemporaryDirectory()
-        self.data_file = os.path.join(self.test_dir.name, "raw_data.txt")
-        with open(self.data_file, "w") as f:
-            f.write("\n")
-
-    def tearDown(self):
-        self.test_dir.cleanup()
 
     def test_scrub_line(self):
         line_in_1 = "[b]Drotik[/b] (1s,6p/mo) (Currently Questing)"
@@ -32,3 +24,26 @@ class TestScrub(unittest.TestCase):
         line_in_2 = "[spoiler] [i]Fighter[/i] 3 (HD 4, hp 5, SD 0, sp 0)."
         expected_line_2 = "Fighter 3 (HD 4, hp 5, SD 0, sp 0)"
         self.assertEqual(expected_line_2, pd.scrub_line(line_in_2))
+
+
+class TestListFromFile(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.TemporaryDirectory()
+        self.data_file = os.path.join(self.test_dir.name, "raw_data.txt")
+        with open(self.data_file, "w") as f:
+            f.write("[b]Drotik[/b] (1s,6p/mo) (Currently Questing)\n")
+            f.write("\n")
+            f.write("[i]Combat[/i]\n")
+
+    def tearDown(self):
+        self.test_dir.cleanup()
+
+    def test_has_right_len(self):
+        expected = 2
+        result = len(pd.list_from_file(self.data_file))
+        self.assertEqual(expected, result)
+
+    def test_has_content(self):
+        result = pd.list_from_file(self.data_file)
+        self.assertIn("Drotik", result[0])
+        self.assertIn("Combat", result[1]) 
